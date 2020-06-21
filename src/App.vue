4c6a16eaa2c27ea3721fd7403bb0ea073ace8b16
LuisSempre <luis.silva@viannasempre.com.br>
2020-06-21 19:05:50 -0300

    Two monitors completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row mt-5">
+      <div class="col text-center">
+        <h1>COVID Monitors</h1>
+      </div>
+    </div>
+      <div class="row mt-5" v-if="arrPositive.length > 0">
+        <div class="col">
+          <h2>Positive</h2>
+          <line-chart :chartData="arrPositive" :options="chartOptions" label="Positive"></line-chart>
+        </div>
+      </div>
+       <div class="row mt-5" v-if="arrPositive.length > 0">
+        <div class="col">
+          <h2>Deaths</h2>
+          <line-chart :chartData="arrDeaths" :options="chartOptions" label="Deaths"></line-chart>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import moment from "moment";
+import LineChart from "./components/LineChart.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LineChart
+    
+  },
+  data() {
+    return{
+        arrPositive: [],
+        arrHopitalized: [],
+        arrInIcu: [],
+        arrOnVentilators:[],
+        arrRecovered: [],
+        arrDeaths: [],
+        chartOptions:{
+          responsive: true,
+          maintainAspectRatio: false
+        }
+    };
+  },
+  async created(){
+    const { 
+      data 
+    } = await axios.get("https://covidtracking.com/api/us/daily");
+
+    data.forEach( d => {
+      const date = moment(d.date, "YYYY-MM-DD").format('MM/DD');
+
+      const {
+        positive, 
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death
+      } = d;
+
+      this.arrPositive.push({ date, total: positive});
+      this.arrHopitalized.push({ date, total: hospitalizedCurrently});
+      this.arrInIcu.push({ date, total: inIcuCurrently});
+      this.arrOnVentilators.push({ date, total: onVentilatorCurrently});
+      this.arrRecovered.push({ date, total: recovered});
+      this.arrDeaths.push({ date, total: death});
+
+      console.log(this.arrPositive);
+    })
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
